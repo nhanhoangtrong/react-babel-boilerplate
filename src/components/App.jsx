8338951583dea0a6d11ad3c67626b5f5e66b61aa
siege250294 <siege.250294@gmail.com>
2016-10-29 00:50:25 +0700

    Add CoffeeScript loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import style from '../css/custom.styl';
 
 const App = React.createClass({
-  render: function() {
-    return (
-      <div className={style.app}>
-        <h1>Hello, World KEndy!</h1>
-      </div>
-    )
-  }
+    getInitialState: function() {
+        return {test: 123};
+    },
+    render: function() {
+        return (
+          <div className={style.app}>
+            <h1>Hello, World KEndy! {this.state.test}</h1>
+          </div>
+        );
+    }
 });
 
 export default App;
