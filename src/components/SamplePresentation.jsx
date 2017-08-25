e066a403a185073a91fa7aba6466a1184744792a
Nhan Hoang <siege250294@gmail.com>
2017-08-25 14:42:06 +0700

    src: test images file-loader and fonts file-loader

--- src/components/SamplePresentation.jsx
+++ src/components/SamplePresentation.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from '@app/stylus/main.styl';
 import SampleStaggeredMotion from './SampleStaggeredMotion';
+import spinner from '@app/img/spinner.gif';
+import fa from 'font-awesome/css/font-awesome.css';
 
 const SamplePresentation = (props) => {
     return (
 		<div className={style.app}>hello aa bb ccss ak ak u that terible ops!!!
-			<button onClick={props.onClickToggleText.bind(props, props.isShownText)}>{props.isShownText ? 'Hide Text' : 'Show Text'}</button>
+			<button onClick={props.onClickToggleText.bind(props, props.isShownText)}>{props.isShownText ? 'Hide Text' : 'Show Text'}<i className={fa.fa + ' ' + fa['fa-flag']} /></button>
 			<SampleStaggeredMotion visible={props.isShownText} />
+            <img src={spinner} alt="Loading..."/>
 		</div>
     );
 };
