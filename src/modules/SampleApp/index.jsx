c94d04caba5f7dba57c689793f2d0e9e6f7f3e89
Nhan Hoang <siege250294@gmail.com>
2018-08-31 10:53:34 +0700

    chore: prettify source code

--- src/modules/SampleApp/index.jsx
+++ src/modules/SampleApp/index.jsx
@@ -16,4 +16,7 @@ const mapDispatchToProps = (dispatch) => ({
         }
     },
 });
-export default connect(mapStateToProps, mapDispatchToProps)(SamplePresentation);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SamplePresentation);
